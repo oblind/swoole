88f33c93a792ee6736d6b29db67df2cbf33d886d
oblind <oblind@163.com>
2019-02-12 17:57:35 +0800

    add view support

--- src/Http/Controller.php
+++ src/Http/Controller.php
@@ -5,10 +5,24 @@ use Swoole\Http\Request;
 use Swoole\Http\Response;
 
 class Controller {
+  /**@var \stdClass $defaultConfig */
+  static $defaultConfig;
+  /**@var \stdClass $config */
+  public $config;
   /**@var Request $request */
   public $request;
   /**@var Response $response */
   public $response;
   /**@var Route $route */
   public $route;
+
+  function __construct($config = null) {
+    $this->config = $config ?? static::$defaultConfig;
+  }
+
+  function view(string $filename) {
+    $this->response->sendfile("{$this->config->viewPath}/$filename");
+  }
 }
+
+Controller::$defaultConfig = (object)['viewPath' => './view'];
--- test/controller/IndexController.php
+++ test/controller/IndexController.php
@@ -3,18 +3,6 @@ use Oblind\Http\Controller;
 
 class IndexController extends Controller {
   function indexAction() {
-    var_dump($this->route->params);
-    $this->response->end('<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <a href="/test">test</a>
-</body>
-</html>
-');
+    $this->view('index.htm');
   }
 }
