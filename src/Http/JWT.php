6f30ceed6cce18a079eeaed5c064755e7bd0c147
oblind <oblind@163.com>
2019-03-26 07:55:15 +0800

    add JWT class

---
+++ src/Http/JWT.php
@@ -0,0 +1,17 @@
+<?php
+namespace Oblind\Http;
+
+class JWT {
+  static function encode($o, string $password): string {
+    return base64url_encode(json_encode($o) . '.' . crypt($password, md5(rand(0x7fff, 0xffff))));
+  }
+
+  static function decode(string $s, $password) {
+    $s = base64url_decode($s);
+    if($p = strpos($s, '.')) {
+      $t = substr($s, $p + 1);
+      if(crypt($password, $t) == $t)
+        return json_decode(substr($s, 0, $p));
+    }
+  }
+}
