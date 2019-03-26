759d07db20095fa52c4b0be733ca95609f3fb897
oblind <oblind@163.com>
2019-03-26 10:33:11 +0800

    class constant

--- src/Http/Port.php
+++ src/Http/Port.php
@@ -5,13 +5,13 @@ use Swoole\Http\Request;
 use Swoole\Http\Response;
 use Swoole\WebSocket\Server;
 
-const RES_BAD_REQUEST = 400;
-const RES_NO_PERMISSION = 401;
-const RES_FORBIDEN = 403;
-const RES_NOT_FOUND = 404;
-const RES_NOT_ALLOWED = 405;
-
 class Port {
+  const RES_BAD_REQUEST = 400;
+  const RES_NO_PERMISSION = 401;
+  const RES_FORBIDEN = 403;
+  const RES_NOT_FOUND = 404;
+  const RES_NOT_ALLOWED = 405;
+  
   /**@var \Swoole\Websocket\Server */
   public $svr;
   /**@var string */
