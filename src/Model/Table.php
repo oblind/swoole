d6ff46c2e01e560f73e501674c2d950c8ebdc4da
oblind <oblind@163.com>
2019-12-01 03:08:50 +0800

    param adjustment

--- src/Model/Table.php
+++ src/Model/Table.php
@@ -4,7 +4,7 @@ namespace Oblind\Model;
 use Oblind\Model\BaseModel;
 
 class Table extends BaseModel {
-  
+
   function __construct($name) {
     if(is_string($name)) {
       static::$tableNames[get_called_class()] = $name;
@@ -16,7 +16,7 @@ class Table extends BaseModel {
   /**
    * return a new BaseTable with table named $name
    */
-  static function open(string $name) {
-    return new static($name);
+  static function open(string $tableName) {
+    return new static($tableName);
   }
 }
