ad53b6fc04e7e7a8903d546365a7abc089a663eb
oblind <oblind@163.com>
2020-10-23 14:27:33 +0800

    ATTR_EMULATE_PREPARES => false

--- src/Model/BaseModel.php
+++ src/Model/BaseModel.php
@@ -62,7 +62,7 @@ class BaseModel extends Decachable implements \JsonSerializable, \IteratorAggreg
           //返回对象, FETCH_ASSOC: 返回数组
           \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
           //不对数字转化字符串
-          //\PDO::ATTR_EMULATE_PREPARES => false,
+          \PDO::ATTR_EMULATE_PREPARES => false,
           //抛出异常
           \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
         ]
